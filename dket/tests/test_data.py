1f657361ef5713b022318ed81723eeda48a09887
petrux <giulio.petrucci@gmail.com>
2017-04-24 18:32:13 +0200

    added examples encoding into protobufs

---
+++ dket/tests/test_data.py
@@ -0,0 +1,42 @@
+"""Test suite for the `dket.data` module."""
+
+import tensorflow as tf
+
+from dket import data
+
+class TestEncode(tf.test.TestCase):
+    """Test case for the `dket.data.encode` function."""
+
+    def test_base(self):
+        """Base test for the `dket.data.encode` function."""
+
+        input_ = [1, 2, 3, 0]
+        output = [12, 23, 34, 45, 0]
+        example = data.encode(input_, output)
+        context_features = example.context.feature
+        feature_lists = example.feature_lists.feature_list
+
+        sentence_length = context_features[data.SENTENECE_LENGTH_KEY].int64_list.value
+        self.assertEquals(1, len(sentence_length))
+        self.assertEquals(len(input_), sentence_length[0])
+
+        formula_length = context_features[data.FORMULA_LENGTH_KEY].int64_list.value
+        self.assertEquals(1, len(formula_length))
+        self.assertEquals(len(output), formula_length[0])
+
+        words = feature_lists[data.WORDS_KEY].feature
+        self.assertEquals(len(input_), len(words))
+        for idx, word in zip(input_, words):
+            feature = word.int64_list.value
+            self.assertEquals(1, len(feature))
+            self.assertEquals(idx, feature[0])
+
+        formula = feature_lists[data.FORMULA_KEY].feature
+        self.assertEquals(len(output), len(formula))
+        for idx, term in zip(output, formula):
+            feature = term.int64_list.value
+            self.assertEquals(1, len(feature))
+            self.assertEquals(idx, feature[0])
+
+if __name__ == '__main__':
+    tf.test.main()

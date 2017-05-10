f5803a9e9225a888aa261946d801fe03aac22fba
petrux <giulio.petrucci@gmail.com>
2017-05-10 12:42:03 +0200

    you cannot activate venv from .sh, so the scripts are just for creation.

--- bin/py2venv.sh
+++ bin/py2venv.sh
@@ -3,9 +3,10 @@
 # if .py2venv doesn't exists, create it,
 # otherwise activate the virtual environment
 if [ -d .py2venv ]; then
-    source .py2venv/bin/activate
+    echo '.py2env already exists!'
 else
     virtualenv .py2venv --python=python2
     source .py2venv/bin/activate
-    pip install -r venvrequirements.txt        
+    pip install -r venvrequirements.txt
+    deactivate
 fi
--- bin/py3venv.sh
+++ bin/py3venv.sh
@@ -3,9 +3,10 @@
 # if .py3venv doesn't exists, create it,
 # otherwise activate the virtual environment
 if [ -d .py3venv ]; then
-    source .py3venv/bin/activate
+    echo '.py3env already exists!'
 else
     virtualenv .py3venv --python=python3
     source .py3venv/bin/activate
     pip install -r venvrequirements.txt        
+    deactivate
 fi

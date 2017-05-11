0dd2b764a1cfe270ec247ecf95240f844f51926f
petrux <giulio.petrucci@gmail.com>
2017-05-11 21:05:55 +0200

    added Metrics (and tests).

--- dket/models/model.py
+++ dket/models/model.py
@@ -149,6 +149,12 @@ class Metrics(object):
     """A function used to judge performances of the model."""
 
     def __init__(self, metrics):
+        """Initialize a new instance.
+
+        Arguments:
+          metrics: a list of callable objects implementing the
+            same signature of the `Metric.compute` method.
+        """
         self._metrics = metrics
 
     def __call__(self, truth, predicted):
@@ -157,12 +163,28 @@ class Metrics(object):
 
     def compute(self, truth, predicted):
         """Compute a set of evaluation metrics on the results.
+
+        Arguments:
+          truth: a `Tensor` representing the gold truth.
+          predicted:  a `Tensor` of same shape and dtype than `truth` representing
+            actual predictions.
+
+        Returns:
+          a `list` of `Op` objects representing the evaluation metrics for the model.
         """
-        return itertools.chain(*[m(truth, predicted) for m in self._metrics])
+        return list(itertools.chain(*[m(truth, predicted) for m in self._metrics]))
 
     @staticmethod
     def batch_mean_accuracy():
-        """Compute the mean accuracy on a batch."""
+        """Compute the mean accuracy on a batch.
+
+        Returns:
+          a `Metrics` instance that returns a list with one `Op` representing
+          the average accuracy of the prediction w.r.t. the gold truth. Both
+          the `truth` and `predicted` tensors are intended to be of type `tf.int32`
+          and to have the same shape and are intended to be sparse representation
+          of classification labels.
+        """
         def _accuracy(truth, predicted):
             acc = tf.reduce_mean(
                 tf.cast(
@@ -254,6 +276,7 @@ class BaseModel(object):
     def target(self):
         """A tensor representing the target output of the model."""
         return self._target
+
     @property
     def logits(self):
         """Unscaled log propabilities."""
--- dket/tests/models/test_model.py
+++ dket/tests/models/test_model.py
@@ -258,3 +258,54 @@ class TestOptimizer(tf.test.TestCase):
         # list of tuple of clipped gradients and variables.
         optimizer.apply_gradients.assert_called_once_with(
             [(grad_x, var_x), (grad_y, var_y)], global_step=global_step)
+
+
+class TestMetrics(tf.test.TestCase):
+    """Test case for the `dket.models.Metrics` class."""
+
+    def test_default(self):
+        """Test the default behaviour of the `dket.models.Metrics` class."""
+
+        truth = tf.constant([0, 1, 2], dtype=tf.int32, name='truth')
+        preds = tf.constant([9, 23, 47], dtype=tf.int32, name='preds')
+
+        metrics_a = mock.Mock()
+        metrics_a_res_01 = tf.no_op(name='A01')
+        metrics_a_res_02 = tf.no_op(name='A02')
+        metrics_a.side_effect = [[metrics_a_res_01, metrics_a_res_02]]
+
+        metrics_b = mock.Mock()
+        metrics_b_res_01 = tf.no_op(name='B01')
+        metrics_b_res_02 = tf.no_op(name='B02')
+        metrics_b.side_effect = [[metrics_b_res_01, metrics_b_res_02]]
+
+        expected = [metrics_a_res_01, metrics_a_res_02,
+                    metrics_b_res_01, metrics_b_res_02]
+        expected = sorted(expected, key=lambda item: item.name)
+
+        metrics = model.Metrics([metrics_a, metrics_b])
+        results = metrics.compute(truth, preds)
+        results = sorted(results, key=lambda item: item.name)
+
+        self.assertEqual(expected, results)
+        metrics_a.assert_called_once_with(truth, preds)
+        metrics_b.assert_called_once_with(truth, preds)
+
+    def test_accuracy(self):
+        """Test the instance created via the `batch_mean_accuracy` factory method."""
+
+        truth = tf.constant([0, 1, 2, 1, 0], dtype=tf.int32, name='truth')
+        preds = tf.constant([1, 1, 2, 3, 0], dtype=tf.int32, name='preds')
+        expected = 3.0 / 5.0
+
+        results = model.Metrics.batch_mean_accuracy().compute(truth, preds)
+        self.assertEqual(1, len(results))
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            actual = sess.run(results[0])
+            self.assertAllClose(expected, actual)
+
+
+if __name__ == '__main__':
+    tf.test.main()

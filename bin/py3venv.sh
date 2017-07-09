6daf619ac8e2cb864b509f53f9ef8a6c99a155e5
petrux <giulio.petrucci@gmail.com>
2017-07-09 15:53:12 +0200

    Updated development installation procedure: 1. only python3 is supported (it's 2017!) 2. you can choose at installation if you want to install the tensorflow-gpu version by running ./bin/py3venv gpu 3. venvrequirements.txt -> requirements.txt 4. added ipython==6.1.0 and pylint==1.7.2 to requirements.

--- bin/py3venv.sh
+++ bin/py3venv.sh
@@ -1,12 +1,31 @@
 # !/bin/bash
 
+PYTHON=python3
+VENV_DIR=.py3venv
+REQUIREMENTS=requirements.txt
+DEVICE=$1
+TF_VERSION='tensorflow'
+
+if [[ $1 == 'gpu' ]]; then
+    TF_VERSION='tensorflow-gpu'
+else
+    TF_VERSION='tensorflow'
+fi
+
 # if .py3venv doesn't exists, create it,
 # otherwise activate the virtual environment
-if [ -d .py3venv ]; then
-    echo '.py3env already exists!'
-else
-    virtualenv .py3venv --python=python3
-    source .py3venv/bin/activate
-    pip install -r venvrequirements.txt --process-dependency-links
-    deactivate
+if [ -d $VENV_DIR ]; then
+    echo $VENV_DIR 'already exists: deleting.' 
+    rm -rf $VENV_DIR
 fi
+
+# else
+echo 'creating virtual env...'
+virtualenv $VENV_DIR --python=$PYTHON
+source $VENV_DIR/bin/activate
+echo 'installing tensorflow version: '$TF_VERSION
+pip install $TF_VERSION
+pip install -r $REQUIREMENTS --process-dependency-links
+echo 'done'
+deactivate
+
